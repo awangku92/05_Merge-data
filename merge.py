7a84cab25c86743f9dcf721cabada149133dfea3
Awang <awangku.safwan92@gmail.com>
2019-07-05 12:26:12 +0800

    Add multiple function to merge data
    merge_files_horizontal()
    merge_file_on_matching_col()
    remove_duplicate_col()

--- merge.py
+++ merge.py
@@ -27,9 +27,47 @@ def merge_files():
         tempDf.append(df)
 
     # merge file and save to .txt file
-    mergeDf = pd.concat(tempDf,axis=0)
+    mergeDf = pd.concat(tempDf,axis=0) # axis=0 ==> merge vertical 
     # print(mergeDf)
     mergeDf.to_csv(mergedPath, index=None)
+
+    return
+
+def merge_file_horizontal():
+    filelist=glob.glob("C:\\Exercise\\*.xls")
+    dfList=[]
+    for filename in filelist:
+        df=pd.read_excel(filename,skiprows=[0,1,2])
+        dfList.append(df)
+        concatDf=pd.concat(dfList,axis=1) # axis=1 ==> merge horizontal
+        concatDf.to_csv("C:\\Exercise\\IncomeByStateByYear.csv",index=0)
+
+    return
+
+def merge_file_on_matching_col():
+    leftDf=pd.read_csv("<file_path>")
+
+    # converters convert data to required datatype
+    rightDf=pd.read_csv("<file_path>", converters={"col1":str, "col2":str})  
+
+    # create new column
+     rightDf["new_col"] = rightDf["col1"]+"-"+rightDf["col2"]
+
+     # df.ix is deprecated, use iloc[] @ loc[] instead - is used for splicing df 
+     #  left_on & right_on is used to merge on that col 
+     mergeDf=pd.merge(leftDf,rightDf.ix[:,["new_col","col3","col4"]], left_on="col", right_on="new_col")
+     # set specified col as index/begining of col 
+     mergeDf.set_index(["col","new_col"], inplace=True)
+     mergeDf.to_csv("<output_file>")
+
+    return
+
+def remove_duplicate_col():
+    concatDf=pd.read_csv("C:\\Exercise\\IncomeByStateByYear.csv")
+    #remove dups
+    # drop_duplicates removes rows, to remove col, use T method to transpose df, remove cols, then transpose again
+    nodupl=concatDf.T.drop_duplicates().T
+    nodupl.to_csv("C:\\Exercise\\IncomeByStateByYearNoDupl.csv",index=0)
 
     return
 
@@ -37,6 +75,9 @@ def merge_files():
 # main method
 def main():
     merge_files()
+    # merge_files_horizontal()
+    # merge_file_on_matching_col()
+    # remove_duplicate_col()
 
 if __name__ == '__main__':
     main()
